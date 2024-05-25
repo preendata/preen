525d501a04cf84f9090f824e78f6a0c03d66443a
Scotty Pate <scotty.pate@auth0.com>
2024-05-25 08:05:56 -0500

    Add concurrency to join reducer

--- internal/engine/join.go
+++ internal/engine/join.go
@@ -3,6 +3,7 @@ package engine
 import (
 	"maps"
 	"reflect"
+	"sync"
 
 	"github.com/xwb1989/sqlparser"
 )
@@ -22,22 +23,30 @@ func (p *ParsedQuery) JoinNodeQuery() error {
 }
 
 func (q *Query) JoinReducer() (*Query, error) {
+	var wg sync.WaitGroup
 	// operator := q.Nodes[0].JoinDetails.Condition.On.(*sqlparser.ComparisonExpr).Operator
 	leftColumn := q.Nodes[0].JoinDetails.Condition.On.(*sqlparser.ComparisonExpr).Left.(*sqlparser.ColName).Name
 	rightColumn := q.Nodes[0].JoinDetails.Condition.On.(*sqlparser.ComparisonExpr).Right.(*sqlparser.ColName).Name
 
 	for idx := range q.Nodes {
+		wg.Add(1)
 		leftRows := q.Nodes[idx].NodeResults[q.Nodes[idx].JoinDetails.LeftTableName]
-		for _, leftRow := range leftRows {
-			rightRows := q.Nodes[idx].NodeResults[q.Nodes[idx].JoinDetails.RightTableName]
-			for _, rightRow := range rightRows {
-				if reflect.DeepEqual(leftRow[leftColumn.String()], rightRow[rightColumn.String()]) {
-					maps.Copy(leftRow, rightRow)
-					q.Results = append(q.Results, leftRow)
+
+		go func(idx int) {
+			for _, leftRow := range leftRows {
+				rightRows := q.Nodes[idx].NodeResults[q.Nodes[idx].JoinDetails.RightTableName]
+				for _, rightRow := range rightRows {
+					if reflect.DeepEqual(leftRow[leftColumn.String()], rightRow[rightColumn.String()]) {
+						maps.Copy(leftRow, rightRow)
+						q.Results = append(q.Results, leftRow)
+					}
 				}
 			}
-		}
+			defer wg.Done()
+		}(idx)
 	}
+
+	wg.Wait()
 
 	return q, nil
 }
