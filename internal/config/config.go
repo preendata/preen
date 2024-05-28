ca5f8485599d88ee2a0bcfafdf4a2087818eba3a
Paulo Black <paulo@pauloblack.com>
2024-05-28 08:56:13 -0400

    stubbed out ensureConfigExists

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"os/user"
 	"path/filepath"
 	"regexp"
 
@@ -17,34 +18,38 @@ var GlobalConfig Config
 
 // Defining our own initialization function because the logic is complex enough that I need to be able to return errors
 func Initialize() error {
-
 	err := godotenv.Load()
 	if err != nil {
 		logger.Warnf("warn: error loading .env file: %v", err)
 	}
 
+	// Initialize logger
 	logLevel := os.Getenv("HYPHADB_LOG_LEVEL")
 
 	if logLevel == "" {
 		logLevel = "ERROR"
 	}
 
-	// Set the log level
 	level, err := logrus.ParseLevel(logLevel)
 	if err != nil {
 		return fmt.Errorf("invalid log level: %v", err)
 	}
 	logger.SetLevel(level)
+	// End intialize logger
 
 	configDirectoryPath := os.Getenv("HYPHADB_CONFIG_PATH")
 
 	// Default to home directory if none set by user
+	// Get the current user's home directory
+	usr, err := user.Current()
+	if err != nil {
+		return fmt.Errorf("failed to get current user: %v", err)
+	}
 	if configDirectoryPath == "" {
-		configDirectoryPath = "~/.hyphadb/" // fallback to default path
-		//TODO this probably needs to be set in a more flexible way than just hardcoding ~/.
-		// Windows? Who cares?
+		configDirectoryPath = filepath.Join(usr.HomeDir, ".hyphadb") // fallback to default path
 	}
 
+	logger.Debugf("Ensuring configuration exists: %s", configDirectoryPath)
 	err = ensureConfigExists(configDirectoryPath)
 
 	if err != nil {
@@ -61,15 +66,27 @@ func Initialize() error {
 }
 
 func ensureConfigExists(configDirectoryPath string) error {
-	dir := filepath.Dir(configDirectoryPath)
+	logger.Debugf("Checking if directory exists: %s", configDirectoryPath)
 
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		// Create the directory
-		err = os.MkdirAll(dir, 0755)
+	// If no directory, create
+	if _, err := os.Stat(configDirectoryPath); os.IsNotExist(err) {
+		logger.Debugf("Directory does not exist, creating it: %s", configDirectoryPath)
+		err = os.MkdirAll(configDirectoryPath, 0755)
+
 		if err != nil {
 			return fmt.Errorf("failed to create directory: %s", err)
 		}
+	} else if err != nil {
+		return fmt.Errorf("error checking directory: %s", err)
+	} else {
+		logger.Debugf("Directory already exists: %s", configDirectoryPath)
 	}
+
+	// If no database config, create
+	// TODO
+
+	// If no DDL config, create
+	// TODO
 
 	return nil
 }
