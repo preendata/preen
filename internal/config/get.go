b748e0d90d2aee6c0e251848ad549dbb4a6c7897
Scotty Pate <scotty.pate@auth0.com>
2024-07-20 04:18:10 -0500

    Add license key validation

--- internal/config/get.go
+++ internal/config/get.go
@@ -8,8 +8,17 @@ import (
 )
 
 func GetConfig() (Config, error) {
-	file, err := os.ReadFile(SingleConfigPath)
+	licenseKey, ok := os.LookupEnv("HYPHADB_LICENSE_KEY")
+	if !ok {
+		return Config{}, fmt.Errorf("HYPHADB_LICENSE_KEY environment variable not set")
+	}
 
+	err := validateLicenseKey(licenseKey)
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to validate license key: %w", err)
+	}
+
+	file, err := os.ReadFile(SingleConfigPath)
 	if err != nil {
 		return Config{}, fmt.Errorf("failed to read config file: %s", err)
 	}
@@ -17,11 +26,11 @@ func GetConfig() (Config, error) {
 	c := Config{}
 
 	err = yaml.Unmarshal(file, &c)
-
 	if err != nil {
 		return Config{}, fmt.Errorf("failed to parse config file: %w", err)
 	}
 
+	// Override config with environment variables
 	fromEnv(&c)
 
 	return c, nil
