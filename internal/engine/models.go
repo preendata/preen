31d534bbebad9a12c01aef22a60720643e75036d
Scotty Pate <scottypate@me.com>
2024-09-23 09:42:02 -0500

    Go linting

--- internal/engine/models.go
+++ internal/engine/models.go
@@ -1,7 +1,6 @@
 package engine
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -98,7 +97,7 @@ func GetModelConfigs(modelTarget string) (*ModelConfig, error) {
 			return nil, fmt.Errorf("error creating models.yaml file at %s with error %s", configFilePath, err)
 		}
 
-		return nil, errors.New(fmt.Sprintf("created empty models.yaml file at %s. Please configure valid models before proceeding", configFilePath))
+		return nil, fmt.Errorf("created empty models.yaml file at %s. Please configure valid models before proceeding", configFilePath)
 	} else if err != nil {
 		return nil, fmt.Errorf("error reading models.yaml file at %s with error %s", configFilePath, err)
 	}
